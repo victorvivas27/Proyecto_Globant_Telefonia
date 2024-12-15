85dd6601202ebc21b9cbb4886d3bbb17b8a9243c
Victor Javier Vivas <victorjaviervivas@gmail.com>
2024-12-15 09:17:52 -0300

    Servicio eliminar

---
+++ src/main/java/com/telefonia_vivas/service/servicioservice/ServicioDeleteService.java
@@ -0,0 +1,25 @@
+package com.telefonia_vivas.service.servicioservice;
+
+import com.telefonia_vivas.exception.ResourceNotFoundException;
+import com.telefonia_vivas.repository.ServicioRepository;
+import com.telefonia_vivas.service.validation.validadorservicio.ValidadorServicio;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class ServicioDeleteService {
+
+    private final ModelMapper modelMapper;
+    private final ServicioRepository servicioRepository;
+    private final ValidadorServicio validadorServicio;
+
+    public void eliminarServicio(Long idServicio) throws ResourceNotFoundException {
+        validadorServicio.validarIdServicio(idServicio);
+
+        servicioRepository.deleteById(idServicio);
+    }
+}
